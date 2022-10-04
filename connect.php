2cb05582790d4b6a18bc9a6bc6cded49c129010d
dominic1515 <dgueccia@gmail.com>
2022-10-04 14:26:50 -0400

    removed useless file

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbname = 'ingredientdb';
-    $dbuser = 'root';
-    $dbpass = 'pass1234';
-    $dbhost = 'localhost';
-    $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
-    mysqli_select_db($link, $dbname) or die("Could not open database '$dbname'");
-?>
