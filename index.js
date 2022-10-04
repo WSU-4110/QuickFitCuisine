fd0040ba994f73f562e4ba28f3e6b626178009d9
gb3896 <gb3896@wayne.edu>
2022-10-03 21:55:49 -0400

    Changed one link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
             <nav>
                 <ul>
-                    <li><a href="ingredients.html">Recipe Finder</a></li>
+                    <li><a href="ingredients.php">Recipe Finder</a></li>
                     <li><a href="SpecialHealthPage.html">Health Issues/Dietary Preferences</a></li>
                 </ul>
             </nav>
