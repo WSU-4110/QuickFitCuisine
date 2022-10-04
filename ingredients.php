ac71ba76596dc6ecb3aad9441b17194879f73c1f
dominic1515 <dgueccia@gmail.com>
2022-10-04 00:53:03 -0400

    very basic filter implemented

--- ingredients.php
+++ ingredients.php
@@ -15,7 +15,7 @@
 	<div class="split left">
   	<div class="centered">
 		<h1>Recipe Finder</h1>
-		<form action="ingredients.php" method="POST">
+		<form action="" method="post">
 			<fieldset>
 				<legend>Ingredient Selector</legend>
 				<!--Ingredient Button Menu, name is array, id is for label, value is string-->
@@ -35,7 +35,7 @@
 
 
 				<!--Submit button, clicked after user selects all ingredients. Recipes will load after this button is clicked.-->
-				<br><input type="submit" value="Find Recipe">
+				<br><input type="submit" name ="submit" value="Find Recipe">
 				
 			</fieldset>
 			
@@ -47,22 +47,31 @@
 	<div class="split right">
   	<div class="centered">
     	<h2>Your Recipes</h2>
-    	<p>Add ingredients to your pantry to see available recipes.
+    	<p>Add ingredients to your pantry to see available recipes.<br>
 		<?php 
     				$dbname = 'ingredientdb';
 					$dbuser = 'root';
 					$dbpass = 'pass1234';
 					$dbhost = 'localhost';
 					$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-					if (isset($_POST['selection'])) {
-						$sql = "SELECT link FROM recipes";
+					if (isset($_POST['submit']) && !empty($_POST['submit'])) {
+						$sql = "SELECT * FROM recipes";
 						$result = $conn->query($sql);
-
+						$ingredients = $_POST['selection'];
 						if ($result->num_rows > 0) {
-							// output data of each row
+							// output link
 							while($row = $result->fetch_assoc()) {
-								$link = $row["link"];
-							  	echo "<a href=$link target=_blank>recipe</a>";
+								for($i=0; $i < count($ingredients); $i++) {
+									if($row["ingredient1"] == $ingredients[$i]) {
+										echo "<a href=$link target=_blank>recipe</a>";
+									}
+									else if($row["ingredient2"] == $ingredients[$i]) {
+										echo "<a href=$link target=_blank>recipe</a>";
+									}
+									else if($row["ingredient3"] == $ingredients[$i]) {
+										echo "<a href=$link target=_blank>recipe</a>";
+									}
+								}								
 							}
 						  } else {
 							echo "0 results";
@@ -71,6 +80,9 @@
 						
 						//$result = $stmt->result_metadata();
 						
+					}
+					else {
+						echo 'waiting';
 					}
 				?>
     	</p>
