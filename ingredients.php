b65177aa259a5850f32a24923f919ab1ef7515e6
gb3896 <gb3896@wayne.edu>
2022-11-01 18:57:39 -0400

    Added icon

--- ingredients.php
+++ ingredients.php
@@ -13,6 +13,10 @@ $_SESSION['ingredientsSortBool'] = false;
 	<title>Quick Fit Cuisine</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="styles.css" rel="stylesheet" type="text/css" />
+	<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+	<link rel="manifest" href="images/site.webmanifest">
 	<script src="index.js" type="text/javascript" defer></script>
 </head>
 
