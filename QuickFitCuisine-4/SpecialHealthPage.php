8bb99821e9f651e70e76c4b7662057075e3e5996
SyedNaumanTanveer <gj5544@wayne.edu>
2022-11-07 18:10:02 -0500

    Adapter Design Pattern

--- QuickFitCuisine-4/SpecialHealthPage.php
+++ QuickFitCuisine-4/SpecialHealthPage.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <title>Lifestyle Options</title>
-    <script src="index.js" type="text/javascript" defer></script>
+    
     <link href="styles.css" rel="stylesheet" type="text/css" />
         
 </head>
@@ -13,6 +13,12 @@
 <body>
 
     <main-header></main-header>
+    <nav>
+                <ul>
+                    <li><a href="ingredients.php">Recipe Finder</a></li>
+                    <li><a href="SpecialHealthPage.php">Health Issues/Dietary Preferences</a></li>
+                </ul>
+    </nav>
 
     <br>Lifestyle Options
     <!--style sheet--> <br>
@@ -43,9 +49,43 @@
             </li>
            <!--new addition, link for new html document.-->
         </ul>
-        
+        <br>
         <footer>
-            <p><small>QuickFitCuisine<b></b></small></p>
+            <p>Dark Mode/LightMode 
+            <img src="moon.png" id="icon" style="width:2em; height:2em;"> <br>
+              <br> 
         </footer>
+
+        <script>
+            var icon = document.getElementById("icon");
+
+            if(localStorage.getItem("theme") == null){
+                localStorage.setItem("theme","light");
+            }
+
+
+            let localData = localStorage.getItem("theme");
+
+            if(localData == "light"){
+                icon.src = "moon.png";
+                document.body.classList.remove("dark-theme");
+            }
+            else if(localData == "dark"){
+                icon.src = "sun.png";
+                document.body.classList.add("dark-theme");
+            }
+
+            icon.onclick = function(){
+                document.body.classList.toggle("dark-theme");
+                if(document.body.classList.contain("dark-theme")){
+                    icon.src = "sun.png";
+                    localStorage.setItem("theme","dark");
+                }else{
+                    icon.src = "moon.png"
+                    localStorage.setItem("theme","light");
+                }
+            }
+        </script>
+
     </body>
 </html>
