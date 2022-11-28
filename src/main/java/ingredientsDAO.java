11c2853b8b0df46cd43c9f2c37631e3164c7de00
dominic1515 <dgueccia@gmail.com>
2022-11-28 16:36:40 -0500

    ingredient files

---
+++ src/main/java/ingredientsDAO.java
@@ -0,0 +1,94 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Servlet implementation class Connect
+ */
+@WebServlet("/ingredientsDAO")
+public class ingredientsDAO 
+{
+	private static final long serialVersionUID = 1L;
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	public ingredientsDAO(){}
+	
+	/** 
+	 * @see HttpServlet#HttpServlet()
+     */
+    protected void connect_func() throws SQLException {
+    	//uses default connection to the database
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/recipedb?allowPublicKeyRetrieval=true&useSSL=false&user=john&password=john1234");
+            System.out.println(connect);
+        }
+    }
+    
+    
+    public boolean database_login(String email, String password) throws SQLException{
+    	try {
+    		connect_func("root","pass1234");
+    		String sql = "select * from user where email = ?";
+    		preparedStatement = connect.prepareStatement(sql);
+    		preparedStatement.setString(1, email);
+    		ResultSet rs = preparedStatement.executeQuery();
+    		return rs.next();
+    	}
+    	catch(SQLException e) {
+    		System.out.println("failed login");
+    		return false;
+    	}
+    }
+	//connect to the database 
+    public void connect_func(String username, String password) throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager
+  			      .getConnection("jdbc:mysql://127.0.0.1:3306/recipedb?"
+  			          + "useSSL=false&user=" + username + "&password=" + password);
+            System.out.println(connect);
+        }
+    }
+    
+    protected void disconnect() throws SQLException {
+        if (connect != null && !connect.isClosed()) {
+        	connect.close();
+        }
+    }
+    
+    public ingredients specificIngredients() {
+    	ingredients i = new ingredients();
+    	
+    	
+    	return i;
+    }
+}
