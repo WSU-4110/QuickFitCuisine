893521a9f59b2f962f4bf18234ee84d065daaca5
SyedNaumanTanveer <gj5544@wayne.edu>
2022-12-11 21:48:51 -0500

    assignment5

---
+++ src/main/java/recipesDAOTest.java
@@ -0,0 +1,62 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class recipesDAOTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRecipesDAO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConnect_func() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDatabase_login() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConnect_funcStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListValidRecipes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAllRecipes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDisconnect() {
+		fail("Not yet implemented");
+	}
+
+}
